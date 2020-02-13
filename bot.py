633f088732ff91168d71c8fb9e85881e27af4ef5
Yahye Abdulle <35307367+Yahye-Abdulle@users.noreply.github.com>
2020-02-13 21:34:48 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -170,4 +170,4 @@ async def bomb(ctx, *arg):
     else:
         await ctx.send(f"\n >>> `Wrong channel`")
 
-bot.run("Njc1OTk5OTAxMDI4NzEyNDU4.XkXAOQ.g5QE_lVEjcxXHe66TDT73wPzWgg")
+bot.run("Njc1OTk5OTAxMDI4NzEyNDU4.XkXA5Q.rsAZ1SnKluHVMnfsQZp3TS5MTwU")
